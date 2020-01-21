08153706c303eaabded149de6e2218009d9cef34
M40V <j.mammeri@gmail.com>
2020-01-21 15:14:22 +0100

    Appveyor test

--- python/populse_mia/user_interface/main_window.py
+++ python/populse_mia/user_interface/main_window.py
@@ -751,7 +751,8 @@ class MainWindow(QMainWindow):
                     file_name)
             self.update_recent_projects_actions()
 
-            os.makedirs(self.exPopup.relative_path)
+            if not os.path.exists(self.exPopup.relative_path):
+                os.makedirs(self.exPopup.relative_path)
 
             os.mkdir(data_path)
             os.mkdir(raw_data_path)
