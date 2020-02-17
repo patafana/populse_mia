89aff9a33b1dfeaaac6ee6beb92c696cdf4c4cf2
M40V <j.mammeri@gmail.com>
2020-02-17 11:00:52 +0100

    Remove dev print

--- python/populse_mia/user_interface/pipeline_manager/node_controller.py
+++ python/populse_mia/user_interface/pipeline_manager/node_controller.py
@@ -542,11 +542,11 @@ class NodeController(QWidget):
         else:
             self.main_window.pipeline_manager.run_pipeline_action\
                 .setDisabled(True)
-        #     print('fail')
-        #     print(self.main_window.pipeline_manager.pipelineEditorTabs\
-        # .get_current_editor().node_parameters_tmp)
-        #     print(self.main_window.pipeline_manager.pipelineEditorTabs\
-        # .get_current_editor().node_parameters)
+            # print('fail')
+            # print(self.main_window.pipeline_manager.pipelineEditorTabs\
+            #   .get_current_editor().node_parameters_tmp)
+            # print(self.main_window.pipeline_manager.pipelineEditorTabs\
+            #   .get_current_editor().node_parameters)
 
         self.setLayout(self.v_box_final)
         # print([x.text() for x in self.line_edit_output])
