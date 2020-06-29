2abd7a1ccc10590c060d8beac2448a4b0ad27de5
Denis Rivière <denis.riviere.gm@gmail.com>
2020-06-29 12:10:28 +0200

    fix in database conversion

--- resources/mia/project_8/build_db.py
+++ resources/mia/project_8/build_db.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91654331961fa22b8ee4b80f86e0578cd1857146d24219dc2d0a9c5d05b92e01
-size 95479
+oid sha256:2448742d70c5238392fe91cb85ab7571f5ae83da530cdbb7e570bfca30c387d8
+size 95365
