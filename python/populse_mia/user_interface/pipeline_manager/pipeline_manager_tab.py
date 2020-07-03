9fc8e107ec47bba33d60075212e6f349163bdc15
econdami <econdami@yahoo.fr>
2020-07-03 20:00:30 +0200

    removing the last engine instantiation that causes the run to crash. Closes #174

--- python/populse_mia/user_interface/pipeline_manager/pipeline_manager_tab.py
+++ python/populse_mia/user_interface/pipeline_manager/pipeline_manager_tab.py
@@ -1984,10 +1984,10 @@ class RunProgress(QProgressDialog):
         self.worker.finished.connect(self.close)
         self.worker.start()
         
-        engine \
-            = self.diagramView.get_current_pipeline().get_study_config().engine
-        with engine.study_config.run_lock:
-            engine.study_config.run_interruption_request = True
+        #engine \
+        #    = self.diagramView.get_current_pipeline().get_study_config().engine
+        #with engine.study_config.run_lock:
+        #    engine.study_config.run_interruption_request = True
 class RunWorker(QThread):
     """Run the pipeline"""
 
