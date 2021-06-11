206607c523a6ba9718a0591c967c8f836e23fbe5
Celinnnne <celine18tarabbia@gmail.com>
2021-06-11 13:25:04 +0200

    Anatomist and Mia_viewer are working

--- python/populse_mia/user_interface/data_viewer/data_viewer_tab.py
+++ python/populse_mia/user_interface/data_viewer/data_viewer_tab.py
@@ -36,24 +36,54 @@ class DataViewerTab(Qt.QWidget):
         hlay.addWidget(self.viewers_combo)
         hlay.addStretch(1)
 
-        self.viewers_combo.addItem('Anatomist')
-        #self.viewers_combo.activated.connect(self.viewer_activated)
-        lay.addStretch(1)
-
         self.viewers_combo.addItem('Mia_viewer')
         #self.viewers_combo.activated.connect(self.viewer_activatedbis)
         lay.addStretch(1)
 
+        self.viewers_combo.addItem('Anatomist')
+        #self.viewers_combo.activated.connect(self.viewer_activated)
+        lay.addStretch(1)
+
+        stacks = Qt.QStackedLayout()
+        lay.addLayout(stacks)
+        lay.addStretch(2)
+
+        #Try import both viewers
+        try:
+            module_anatomist = 'populse_mia.user_interface.data_viewer.anatomist'
+            path = os.getcwd()
+            viewer_module_anatomist = importlib.import_module(module_anatomist, os.path.join(path,'user_interface/data_viewer/anatomist/__init__.py'))
+            self.viewer_anatomist = viewer_module_anatomist.MiaViewer()
+
+            module_miaViewer = 'populse_mia.user_interface.data_viewer.mia_viewer'
+            viewer_module_miaViewer = importlib.import_module(module_miaViewer, os.path.join(path,'user_interface/data_viewer/mia_viewer/__init__.py'))
+            self.viewer_miaViewer = viewer_module_miaViewer.MiaViewer()
+
+        except Exception as e:
+            print('\n{0} viewer is not available or not working '
+                  '...!\nTraceback:'.format(viewer_name))
+            print(''.join(traceback.format_tb(e.__traceback__)), end='')
+            print('{0}: {1}\n'.format(e.__class__.__name__, e))
+            return
+
+        #Add Widgets to QStackLayout (defaulf display is the first widget added)
+        stacks.addWidget(self.viewer_anatomist)
+        stacks.addWidget(self.viewer_miaViewer)
+
+        self.stacks = stacks
         self.layout = lay
         self.viewer_name = None
         self.viewer = None
+        self.project = []
+        self.docs = []
 
         self.viewers_combo.currentIndexChanged.connect(self.change_viewer)
 
-        # self.viewer_activated(0)
     def change_viewer(self):
         index = self.viewers_combo.currentIndex()
         self.viewer_activated(index)
+        self.viewer.project = self.project
+        self.viewer.documents = self.docs
 
     def current_viewer(self):
         if self.viewer_name is None:
@@ -71,28 +101,15 @@ class DataViewerTab(Qt.QWidget):
 
         print('- activate viewer:', viewer_name)
 
-        try:
-            viewer_module = importlib.import_module(
-                '%s.%s' % (__name__.rsplit('.', 1)[0], viewer_name))
-            print("vIEWER MODULE")
-            print(viewer_module)
-            viewer = viewer_module.MiaViewer()
-
-        except Exception as e:
-            print('\n{0} viewer is not available or not working '
-                  '...!\nTraceback:'.format(viewer_name))
-            print(''.join(traceback.format_tb(e.__traceback__)), end='')
-            print('{0}: {1}\n'.format(e.__class__.__name__, e))
-            return
-
-        if self.viewer is not None:
-            self.viewer.deleteLater()
-            del self.viewer
-
-        self.viewer_name = viewer_name
+        if viewer_name == 'anatomist':
+            viewer = self.viewer_anatomist
+        else:
+            viewer = self.viewer_miaViewer
+        self.stacks.setCurrentWidget(viewer)
         self.viewer = viewer
-        self.layout.insertWidget(1, viewer)
 
     def set_documents(self, project, documents):
         if self.viewer:
             self.viewer.set_documents(project, documents)
+            self.project = project
+            self.docs = documents
