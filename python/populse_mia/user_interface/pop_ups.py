b8a998577cc8df0cefdf93e69599ae2770b428a7
econdami <econdami@yahoo.fr>
2021-06-14 18:58:44 +0200

    remove typo in pop_ups module

--- python/populse_mia/user_interface/pop_ups.py
+++ python/populse_mia/user_interface/pop_ups.py
@@ -27,10 +27,10 @@
         - PopUpSeeAllProjects
         - PopUpSelectFilter
         - PopUpSelectIteration
+        - PopUpTagSelection  (must precede PopUpSelectTag)
         - PopUpSelectTag
         - PopUpSelectTagCountTable
         - PopUpShowBrick
-        - PopUpTagSelection
         - PopUpVisualizedTags
         - QLabel_clickable
 
@@ -3909,6 +3909,133 @@ class PopUpSelectIteration(QDialog):
         self.close()
 
 
+class PopUpTagSelection(QDialog):
+    """Is called when the user wants to update the tags that are visualized in
+       the data browser.
+
+    .. Methods:
+        - cancel_clicked: closes the pop-up
+        - item_clicked: checks the checkbox of an item when the latter
+          is clicked
+        - ok_clicked: actions when the "OK" button is clicked
+        - search_str: matches the searched pattern with the tags of the project
+
+    """
+
+    def __init__(self, project):
+        """Initialization.
+
+        :param project: current project in the software
+
+        """
+
+        super().__init__()
+        self.project = project
+
+        _translate = QtCore.QCoreApplication.translate
+
+        # The "Tag list" label
+        self.label_tag_list = QtWidgets.QLabel(self)
+        self.label_tag_list.setTextFormat(QtCore.Qt.AutoText)
+        self.label_tag_list.setObjectName("label_tag_list")
+        self.label_tag_list.setText(_translate("main_window",
+                                               "Available tags:"))
+
+        # The search bar to search in the list of tags
+        self.search_bar = QtWidgets.QLineEdit(self)
+        self.search_bar.setObjectName("lineEdit_search_bar")
+        self.search_bar.setPlaceholderText("Search")
+        self.search_bar.textChanged.connect(self.search_str)
+
+        # The list of tags
+        self.list_widget_tags = QtWidgets.QListWidget(self)
+        self.list_widget_tags.setObjectName("listWidget_tags")
+        self.list_widget_tags.setSelectionMode(
+            QtWidgets.QAbstractItemView.SingleSelection)
+        self.list_widget_tags.itemClicked.connect(self.item_clicked)
+
+        self.push_button_ok = QtWidgets.QPushButton(self)
+        self.push_button_ok.setObjectName("pushButton_ok")
+        self.push_button_ok.setText("OK")
+        self.push_button_ok.clicked.connect(self.ok_clicked)
+
+        self.push_button_cancel = QtWidgets.QPushButton(self)
+        self.push_button_cancel.setObjectName("pushButton_cancel")
+        self.push_button_cancel.setText("Cancel")
+        self.push_button_cancel.clicked.connect(self.cancel_clicked)
+
+        hbox_top_left = QHBoxLayout()
+        hbox_top_left.addWidget(self.label_tag_list)
+        hbox_top_left.addWidget(self.search_bar)
+
+        vbox_top_left = QVBoxLayout()
+        vbox_top_left.addLayout(hbox_top_left)
+        vbox_top_left.addWidget(self.list_widget_tags)
+
+        hbox_buttons = QHBoxLayout()
+        hbox_buttons.addStretch(1)
+        hbox_buttons.addWidget(self.push_button_ok)
+        hbox_buttons.addWidget(self.push_button_cancel)
+
+        vbox_final = QVBoxLayout()
+        vbox_final.addLayout(vbox_top_left)
+        vbox_final.addLayout(hbox_buttons)
+
+        self.setLayout(vbox_final)
+
+    def cancel_clicked(self):
+        """Closes the pop-up."""
+
+        self.close()
+
+    def item_clicked(self, item):
+        """Checks the checkbox of an item when the latter is clicked.
+
+        :param item: clicked item
+
+        """
+
+        for idx in range(self.list_widget_tags.count()):
+            itm = self.list_widget_tags.item(idx)
+            if itm == item:
+                itm.setCheckState(QtCore.Qt.Checked)
+            else:
+                itm.setCheckState(QtCore.Qt.Unchecked)
+
+    def ok_clicked(self):
+        """Actions when the "OK" button is clicked."""
+
+        # Has to be override in the PopUpSelectTag* classes
+        pass
+
+    def search_str(self, str_search):
+        """Matches the searched pattern with the tags of the project.
+
+        :param str_search: string pattern to search
+
+        """
+
+        return_list = []
+        if str_search != "":
+            for tag in self.project.session.get_fields_names(
+                    COLLECTION_CURRENT):
+                if tag != TAG_CHECKSUM:
+                    if str_search.upper() in tag.upper():
+                        return_list.append(tag)
+        else:
+            for tag in self.project.session.get_fields_names(
+                    COLLECTION_CURRENT):
+                if tag != TAG_CHECKSUM:
+                    return_list.append(tag)
+
+        for idx in range(self.list_widget_tags.count()):
+            item = self.list_widget_tags.item(idx)
+            if item.text() in return_list:
+                item.setHidden(False)
+            else:
+                item.setHidden(True)
+
+
 class PopUpSelectTag(PopUpTagSelection):
     """Is called when the user wants to update the tag to display in the mini
       viewer.
@@ -4194,133 +4321,6 @@ class PopUpShowBrick(QDialog):
         item_to_scroll_to = self.databrowser.table_data.item(row_to_select, 0)
         self.databrowser.table_data.scrollToItem(item_to_scroll_to)
         self.close()
-
-
-class PopUpTagSelection(QDialog):
-    """Is called when the user wants to update the tags that are visualized in
-       the data browser.
-
-    .. Methods:
-        - cancel_clicked: closes the pop-up
-        - item_clicked: checks the checkbox of an item when the latter
-          is clicked
-        - ok_clicked: actions when the "OK" button is clicked
-        - search_str: matches the searched pattern with the tags of the project
-
-    """
-
-    def __init__(self, project):
-        """Initialization.
-
-        :param project: current project in the software
-
-        """
-
-        super().__init__()
-        self.project = project
-
-        _translate = QtCore.QCoreApplication.translate
-
-        # The "Tag list" label
-        self.label_tag_list = QtWidgets.QLabel(self)
-        self.label_tag_list.setTextFormat(QtCore.Qt.AutoText)
-        self.label_tag_list.setObjectName("label_tag_list")
-        self.label_tag_list.setText(_translate("main_window",
-                                               "Available tags:"))
-
-        # The search bar to search in the list of tags
-        self.search_bar = QtWidgets.QLineEdit(self)
-        self.search_bar.setObjectName("lineEdit_search_bar")
-        self.search_bar.setPlaceholderText("Search")
-        self.search_bar.textChanged.connect(self.search_str)
-
-        # The list of tags
-        self.list_widget_tags = QtWidgets.QListWidget(self)
-        self.list_widget_tags.setObjectName("listWidget_tags")
-        self.list_widget_tags.setSelectionMode(
-            QtWidgets.QAbstractItemView.SingleSelection)
-        self.list_widget_tags.itemClicked.connect(self.item_clicked)
-
-        self.push_button_ok = QtWidgets.QPushButton(self)
-        self.push_button_ok.setObjectName("pushButton_ok")
-        self.push_button_ok.setText("OK")
-        self.push_button_ok.clicked.connect(self.ok_clicked)
-
-        self.push_button_cancel = QtWidgets.QPushButton(self)
-        self.push_button_cancel.setObjectName("pushButton_cancel")
-        self.push_button_cancel.setText("Cancel")
-        self.push_button_cancel.clicked.connect(self.cancel_clicked)
-
-        hbox_top_left = QHBoxLayout()
-        hbox_top_left.addWidget(self.label_tag_list)
-        hbox_top_left.addWidget(self.search_bar)
-
-        vbox_top_left = QVBoxLayout()
-        vbox_top_left.addLayout(hbox_top_left)
-        vbox_top_left.addWidget(self.list_widget_tags)
-
-        hbox_buttons = QHBoxLayout()
-        hbox_buttons.addStretch(1)
-        hbox_buttons.addWidget(self.push_button_ok)
-        hbox_buttons.addWidget(self.push_button_cancel)
-
-        vbox_final = QVBoxLayout()
-        vbox_final.addLayout(vbox_top_left)
-        vbox_final.addLayout(hbox_buttons)
-
-        self.setLayout(vbox_final)
-
-    def cancel_clicked(self):
-        """Closes the pop-up."""
-
-        self.close()
-
-    def item_clicked(self, item):
-        """Checks the checkbox of an item when the latter is clicked.
-
-        :param item: clicked item
-
-        """
-
-        for idx in range(self.list_widget_tags.count()):
-            itm = self.list_widget_tags.item(idx)
-            if itm == item:
-                itm.setCheckState(QtCore.Qt.Checked)
-            else:
-                itm.setCheckState(QtCore.Qt.Unchecked)
-
-    def ok_clicked(self):
-        """Actions when the "OK" button is clicked."""
-
-        # Has to be override in the PopUpSelectTag* classes
-        pass
-
-    def search_str(self, str_search):
-        """Matches the searched pattern with the tags of the project.
-
-        :param str_search: string pattern to search
-
-        """
-
-        return_list = []
-        if str_search != "":
-            for tag in self.project.session.get_fields_names(
-                    COLLECTION_CURRENT):
-                if tag != TAG_CHECKSUM:
-                    if str_search.upper() in tag.upper():
-                        return_list.append(tag)
-        else:
-            for tag in self.project.session.get_fields_names(
-                    COLLECTION_CURRENT):
-                if tag != TAG_CHECKSUM:
-                    return_list.append(tag)
-
-        for idx in range(self.list_widget_tags.count()):
-            item = self.list_widget_tags.item(idx)
-            if item.text() in return_list:
-                item.setHidden(False)
-            else:
-                item.setHidden(True)
 
 
 class PopUpVisualizedTags(QWidget):
