2dce1d92a5beee9fe0e903ecfee4f7545204c59d
econdami <econdami@yahoo.fr>
2021-07-03 11:11:21 +0200

    modularity improvement of the data viewer

--- python/populse_mia/user_interface/data_viewer/data_viewer_tab.py
+++ python/populse_mia/user_interface/data_viewer/data_viewer_tab.py
@@ -9,7 +9,6 @@ Contains:
 """
 
 from soma.qt_gui.qt_backend import Qt
-#import anatomist.direct.api as ana
 import importlib
 import os
 import traceback
@@ -18,23 +17,24 @@ import traceback
 class DataViewerTab(Qt.QWidget):
     '''
     DataViewerTab is the widget in the data viewer tab of Populse-MIA GUI.
-    A combobox containing the available viewers will always appear
-    If import of anatomist and anatomist_2 fails, it won't impact the work of Mia itself
-    Viewers are put in Qt.QStackedLayout in order to share a same project
-    A new viewer can be added
+    A combobox containing the available viewers will always appear.
+    If import of viewers fails, it won't impact the work of Mia itself.
+    Viewers are put in Qt.QStackedLayout in order to share a same project.
+    A new viewer can be added simply by placing it in the data_viewer directory.
     '''
 
     def __init__(self, main_window):
+        """blabla
 
+        :param main_window: blabla
+        """
         super(DataViewerTab, self).__init__()
-
-        self.stacks = []
-        self.lay = []
-        self.viewer_name = None
-        self.viewer = None
-        self.project = []
         self.docs = []
-        self.viewers = []
+        self.lay = []
+        self.project = []
+        self.stacks = []
+        self.viewers_loaded = {}
+        self.viewer_current = {}
 
         #Display of combobox containing the viewers
         self.main_window = main_window
@@ -48,14 +48,14 @@ class DataViewerTab(Qt.QWidget):
         #Combobox will contain the viewers if they are available
         self.viewers_combo = Qt.QComboBox()
         self.viewers_combo.setMinimumWidth(150)
-
+ 
         hlay.addWidget(self.viewers_combo)
         hlay.addStretch(1)
 
         self.viewers_combo.currentIndexChanged.connect(self.change_viewer)
 
     def change_viewer(self):
-        '''switche to viewer selected in the combobox
+        '''switches to viewer selected in the combobox
         pass the project from on viewer to the other
         '''
         index = self.viewers_combo.currentIndex()
@@ -63,77 +63,88 @@ class DataViewerTab(Qt.QWidget):
         self.set_documents(self.project, self.docs)
 
     def current_viewer(self):
-        if self.viewer_name is None:
+        """blabla
+
+        """
+        if not self.viewer_current:
             return self.viewers_combo.currentText().lower()
         else:
-            return self.viewer_name
+            return list(self.viewer_current)[0]
 
     def viewer_activated(self, index):
+        """blabla
+
+        :param index: blabla
+        """
         viewer_name = self.viewers_combo.itemText(index).lower()
         self.activate_viewer(viewer_name)
 
     def load_viewer(self, viewer_name):
+        """blabla
 
-        if self.viewers.__len__() == 0:
+        :param viewer_name: blabla
+        """
+        if viewer_name:
+            detected_viewer = [viewer_name]
+
+        else:
+            detected_viewer = [p for p in os.listdir(os.path.dirname(__file__))
+                               if os.path.isdir(os.path.abspath(os.path.join(
+                                       os.path.dirname(__file__), p)))
+                                   and p != '__pycache__']
+            #detected_viewer = ['anatomist', 'anatomist_2']
+
+        if not self.viewers_loaded:
             self.stacks = Qt.QStackedLayout()
             self.lay.addLayout(self.stacks)
 
         init_global_handlers = True
 
-        #Try import anatomist viewer
-        if 'anatomist' not in self.viewers:
-            try:
-                viewer_name = 'anatomist'
-                viewer_module = importlib.import_module(
-                '%s.%s' % (__name__.rsplit('.', 1)[0], viewer_name))
-                self.viewer_anatomist = viewer_module.MiaViewer()
-                self.stacks.addWidget(self.viewer_anatomist)
-                self.viewers_combo.addItem('Anatomist')
-                self.viewers.append('anatomist')
-                #Check if initialization of controls has been done:
-                if self.viewer_anatomist.anaviewer._global_handlers_initialized:
-                    init_global_handlers = False
+        #Try import detected viewers
+        for viewer_name in detected_viewer:
 
-            except Exception as e:
-                print('\n{0} viewer is not available or not working '
-                      '...!\nTraceback:'.format(viewer_name))
-                print(''.join(traceback.format_tb(e.__traceback__)), end='')
-                print('{0}: {1}\n'.format(e.__class__.__name__, e))
+            if viewer_name not in self.viewers_loaded:
 
-        #Try import anatomist_2
-        if 'anatomist_2' not in self.viewers:
-            try:
-                viewer_name = 'anatomist_2'
-                viewer_module = importlib.import_module(
-                '%s.%s' % (__name__.rsplit('.', 1)[0], viewer_name))
-                self.viewer_anatomist_2 = viewer_module.MiaViewer(init_global_handlers)
-                self.stacks.addWidget(self.viewer_anatomist_2)
-                self.viewers_combo.addItem('Anatomist_2')
-                self.viewers.append('anatomist_2')
+                try:
+                    viewer_module = importlib.import_module(
+                    '%s.%s' % (__name__.rsplit('.', 1)[0], viewer_name))
+                    self.viewers_loaded[viewer_name] = viewer_module.MiaViewer()
+                    self.stacks.addWidget(self.viewers_loaded[viewer_name])
+                    self.viewers_combo.addItem(viewer_name)
+                    #Check if initialization of controls has been done:
+                    if self.viewers_loaded[
+                            viewer_name].anaviewer._global_handlers_initialized:
+                        init_global_handlers = False
 
-            except Exception as e:
-                print('\n{0} viewer is not available or not working '
-                      '...!\nTraceback:'.format(viewer_name))
-                print(''.join(traceback.format_tb(e.__traceback__)), end='')
-                print('{0}: {1}\n'.format(e.__class__.__name__, e))
+                except Exception as e:
+                    print('\n{0} viewer is not available or not working '
+                          '...!\nTraceback:'.format(viewer_name))
+                    print(''.join(traceback.format_tb(e.__traceback__)), end='')
+                    print('{0}: {1}\n'.format(e.__class__.__name__, e))
 
     def activate_viewer(self, viewer_name):
-        viewer = viewer_name
-        if self.viewer_name == viewer_name:
-            return
-        print('- activate viewer:', viewer_name)
+        """blabla
 
-        if viewer_name == 'anatomist':
-            viewer = self.viewer_anatomist
-        if viewer_name == 'anatomist_2':
-            viewer = self.viewer_anatomist_2
+        :param viewer_name: blabla
+        """
+        if self.viewer_current and list(self.viewer_current)[0] == viewer_name:
+            return
+
+        print('\n- activate viewer:', viewer_name)
+        viewer = self.viewers_loaded[viewer_name]
 
         if viewer:
             self.stacks.setCurrentWidget(viewer)
-            self.viewer = viewer
+            self.viewer_current.clear()
+            self.viewer_current[viewer_name] = viewer
 
     def set_documents(self, project, documents):
-        if self.viewer:
-            self.viewer.set_documents(project, documents)
+        """blabla
+
+        :param project: blabla
+        :param documents: blabla
+        """
+        if self.viewer_current:
+            self.viewer_current[list(self.viewer_current)[0]].set_documents(project, documents)
             self.project = project
             self.docs = documents
