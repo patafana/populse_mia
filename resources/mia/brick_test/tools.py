265707cc1c60aad81a49b37533fcb45090ecb080
econdami <econdami@yahoo.fr>
2021-08-27 12:10:02 +0200

    work on unit testing

--- resources/mia/brick_test/tools.py
+++ resources/mia/brick_test/tools.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bdbe8f4eb0def3cb7bc625b28f42ace95fb2f03a5a0187588a77c8848b7237c
-size 5372
+oid sha256:8b3d4dfc756e9389329c720fc4fd49b72c5d076d5cbd269ac1ddae693a27a22e
+size 6561
